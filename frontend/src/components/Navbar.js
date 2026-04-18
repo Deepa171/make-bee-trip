659649ef2e7f64a9061d09a1d36ed72860a8c5af
Abhay6830 <116333233+Abhay6830@users.noreply.github.com>
2026-04-19 00:07:37 +0530

    changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,42 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-dark px-4">
-      <span className="navbar-brand fw-bold fs-4">🐝 Make Bee Trip</span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top px-4">
+      
+      {/* Logo */}
+      <a className="navbar-brand fw-bold" href="#">
+        🐝 Make Bee Trip
+      </a>
+
+      {/* Toggle for mobile */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* Links */}
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Flights</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Bus</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Train</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Hotels</a>
+          </li>
+        </ul>
+
+        {/* CTA Button */}
+        <button className="btn btn-gradient">Book Now</button>
+      </div>
     </nav>
   );
 }
