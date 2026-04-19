c80fcc8fd83e9ae4bd4af84bf62ca76b8640e75f
Abhay6830 <116333233+Abhay6830@users.noreply.github.com>
2026-04-19 09:58:26 +0530

    fixing toggle

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 // v3
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
